f6b0f40f3fb389f07d630f16f4588e9fe63af810
EC2 Default User <ec2-user@ip-172-31-86-167.ec2.internal>
2022-08-05 09:03:02 +0000

    helloo

---
+++ P.java
@@ -0,0 +1,25 @@
+class P
+{
+    public static void main (String[] args) {
+     char a='A'; 
+      for(int i=0;i<=2;i++)
+      {
+          for(int j=0;j<=2;j++)
+      {
+          System.out.print(a);
+          a++;
+      }
+      System.out.println();
+      }
+      
+      
+      
+      
+        
+    }
+    
+    
+    
+    
+    
+}
